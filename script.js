ee29391027630d3ae5cad973635e0e47fd1c02d0
Kira <kirnan_t@outlok.com>
2025-05-06 14:27:51 +0700

    PRODUCT ABD REVIEW

--- script.js
+++ script.js
@@ -6,3 +6,20 @@ function getReviews() {
 function saveReviews(reviews) {
     localStorage.setItem('reviews', JSON.stringify(reviews));
 }
+
+function displayProducts() {
+    const productsContainer = document.getElementById('products-container');
+    productsContainer.innerHTML = '';
+
+    const reviews = getReviews();
+
+    for (const product in reviews) {
+        const productDiv = document.createElement('div');
+        productDiv.className = 'product';
+        productDiv.textContent = product;
+
+        productDiv.addEventListener('click', () => displayReviews(product));
+
+        productsContainer.appendChild(productDiv);
+    }
+}
