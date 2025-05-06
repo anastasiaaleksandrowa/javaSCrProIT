eda5cb4984e2dd95ea3de31a605c488cab1a90d2
Kira <kirnan_t@outlok.com>
2025-05-06 14:25:33 +0700

    saving data

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ function getReviews() {
     const reviews = localStorage.getItem('reviews');
     return reviews ? JSON.parse(reviews) : {};
 }
+
+function saveReviews(reviews) {
+    localStorage.setItem('reviews', JSON.stringify(reviews));
+}
